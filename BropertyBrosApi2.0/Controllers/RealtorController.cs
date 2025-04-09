f79bfeaaaf5d479fad290995517546f97c2c5186
Cal <151396661+CalvinJagare@users.noreply.github.com>
2025-04-09 15:34:13 +0200

    Scaffolded Controller Bro

---
+++ BropertyBrosApi2.0/Controllers/RealtorController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BropertyBrosApi.Data;
+using BropertyBrosApi.Models;
+
+namespace BropertyBrosApi2._0.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RealtorController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RealtorController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Realtor
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Realtor>>> GetRealtors()
+        {
+            return await _context.Realtors.ToListAsync();
+        }
+
+        // GET: api/Realtor/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Realtor>> GetRealtor(int id)
+        {
+            var realtor = await _context.Realtors.FindAsync(id);
+
+            if (realtor == null)
+            {
+                return NotFound();
+            }
+
+            return realtor;
+        }
+
+        // PUT: api/Realtor/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRealtor(int id, Realtor realtor)
+        {
+            if (id != realtor.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(realtor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RealtorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Realtor
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Realtor>> PostRealtor(Realtor realtor)
+        {
+            _context.Realtors.Add(realtor);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRealtor", new { id = realtor.Id }, realtor);
+        }
+
+        // DELETE: api/Realtor/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRealtor(int id)
+        {
+            var realtor = await _context.Realtors.FindAsync(id);
+            if (realtor == null)
+            {
+                return NotFound();
+            }
+
+            _context.Realtors.Remove(realtor);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RealtorExists(int id)
+        {
+            return _context.Realtors.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ BropertyBrosApi2.0/Controllers/RealtorFirmController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BropertyBrosApi.Data;
+using BropertyBrosApi.Models;
+
+namespace BropertyBrosApi2._0.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RealtorFirmController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RealtorFirmController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/RealtorFirm
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RealtorFirm>>> GetRealtorFirms()
+        {
+            return await _context.RealtorFirms.ToListAsync();
+        }
+
+        // GET: api/RealtorFirm/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RealtorFirm>> GetRealtorFirm(int id)
+        {
+            var realtorFirm = await _context.RealtorFirms.FindAsync(id);
+
+            if (realtorFirm == null)
+            {
+                return NotFound();
+            }
+
+            return realtorFirm;
+        }
+
+        // PUT: api/RealtorFirm/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRealtorFirm(int id, RealtorFirm realtorFirm)
+        {
+            if (id != realtorFirm.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(realtorFirm).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RealtorFirmExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/RealtorFirm
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<RealtorFirm>> PostRealtorFirm(RealtorFirm realtorFirm)
+        {
+            _context.RealtorFirms.Add(realtorFirm);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRealtorFirm", new { id = realtorFirm.Id }, realtorFirm);
+        }
+
+        // DELETE: api/RealtorFirm/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRealtorFirm(int id)
+        {
+            var realtorFirm = await _context.RealtorFirms.FindAsync(id);
+            if (realtorFirm == null)
+            {
+                return NotFound();
+            }
+
+            _context.RealtorFirms.Remove(realtorFirm);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RealtorFirmExists(int id)
+        {
+            return _context.RealtorFirms.Any(e => e.Id == id);
+        }
+    }
+}
