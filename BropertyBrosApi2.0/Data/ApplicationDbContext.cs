6c2010af46c288842f6409efce0712dadcda35fc
Cal <151396661+CalvinJagare@users.noreply.github.com>
2025-04-22 11:19:27 +0200

    .

--- BropertyBrosApi2.0/Data/ApplicationDbContext.cs
+++ BropertyBrosApi2.0/Data/ApplicationDbContext.cs
@@ -77,11 +77,11 @@ namespace BropertyBrosApi.Data
                 new Realtor
                 {
                     Id = 1,
-                    FirstName = "Markus",
+                    FirstName = "Marcus",
                     LastName = "Friberg",
                     PhoneNumber = "0705712647",
                     Email = "markus@bropertybros.se",
-                    ProfileUrl = "https://example.com/profiles/markus.png",
+                    ProfileUrl = "https://media.licdn.com/dms/image/v2/D4D03AQEYZfjOaaV_QA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1719018397094?e=1750896000&v=beta&t=7Tc6mYQarQ62J6tfvYWlA5wLSLsxO-x5_eIlfPkYWIw",
                     RealtorFirmId = 1
                 },
                 new Realtor
@@ -91,7 +91,7 @@ namespace BropertyBrosApi.Data
                     LastName = "Mäklarsson",
                     PhoneNumber = "0731234567",
                     Email = "sanna@bropertybros.se",
-                    ProfileUrl = "https://example.com/profiles/sanna.png",
+                    ProfileUrl = "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg",
                     RealtorFirmId = 1
                 },
                 new Realtor
@@ -101,7 +101,7 @@ namespace BropertyBrosApi.Data
                     LastName = "Fast",
                     PhoneNumber = "0704455667",
                     Email = "erik@maklarkompaniet.se",
-                    ProfileUrl = "https://example.com/profiles/erik.png",
+                    ProfileUrl = "https://newprofilepic.photo-cdn.net//assets/images/article/profile.jpg?90af0c8",
                     RealtorFirmId = 2
                 },
                 new Realtor
@@ -111,7 +111,7 @@ namespace BropertyBrosApi.Data
                     LastName = "Sund",
                     PhoneNumber = "0761122334",
                     Email = "anna@fastighetsmastarna.se",
-                    ProfileUrl = "https://example.com/profiles/anna.png",
+                    ProfileUrl = "https://media.istockphoto.com/id/1682296067/photo/happy-studio-portrait-or-professional-man-real-estate-agent-or-asian-businessman-smile-for.jpg?s=612x612&w=0&k=20&c=9zbG2-9fl741fbTWw5fNgcEEe4ll-JegrGlQQ6m54rg=",
                     RealtorFirmId = 3
                 },
                 new Realtor
