32cd9964d9baf91c0974984a61390e99179bdc39
twofourZ <uppenberg95@gmail.com>
2025-05-06 15:31:52 +0200

    propertysearch

--- BropertyBrosApi2.0/DTOs/Properties/PropertySearchDto.cs
+++ BropertyBrosApi2.0/DTOs/Properties/PropertySearchDto.cs
@@ -2,21 +2,21 @@
 {
     public class PropertySearchDto
     {
-        public int? MinPrice { get; set; } = null;
+        public int MinPrice { get; set; }
         public int? MaxPrice { get; set; } = null;
-        public int? MinMonthlyFee { get; set; } = null;
+        public int MinMonthlyFee { get; set; }
         public int? MaxMonthlyFee { get; set; } = null;
-        public int? MinYearlyFee { get; set; } = null;
+        public int MinYearlyFee { get; set; }
         public int? MaxYearlyFee { get; set; } = null;
-        public int? MinLivingAreaKvm { get; set; } = null;
+        public int MinLivingAreaKvm { get; set; }
         public int? MaxLivingAreaKvm { get; set; } = null;
-        public int? MinAncillaryAreaKvm { get; set; } = null;
+        public int MinAncillaryAreaKvm { get; set; }
         public int? MaxAncillaryAreaKvm { get; set; } = null;
-        public int? MinLandAreaKvm { get; set; } = null;
+        public int MinLandAreaKvm { get; set; }
         public int? MaxLandAreaKvm { get; set; } = null;
-        public int? MinNumberOfRooms { get; set; } = null;
+        public int MinNumberOfRooms { get; set; }
         public int? MaxNumberOfRooms { get; set; } = null;
-        public int? MinBuildYear { get; set; } = null;
+        public int MinBuildYear { get; set; }
         public int? MaxBuildYear { get; set; } = null;
         public int? CityId { get; set; } = null;
         public int? CategoryId { get; set; } = null;
