630e5750d3023e80cf220117693be5e8c2b69a5f
Daniel Näslund <daniel.naslund19@gmail.com>
2025-05-07 14:30:10 +0200

    added required to models

--- BropertyBrosApi2.0/Models/Property.cs
+++ BropertyBrosApi2.0/Models/Property.cs
@@ -1,19 +1,32 @@
-﻿namespace BropertyBrosApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BropertyBrosApi.Models
 {
     //Author: Calvin, Daniel, Emil
     public class Property
     {
         public int Id { get; set; }
+        [Required]
         public string? Address { get; set; }
+        [Required]
         public int Price { get; set; }
+        [Required]
         public int MonthlyFee { get; set; }
+        [Required]
         public int YearlyFee { get; set; }
+        [Required]
         public int LivingAreaKvm { get; set; }
+        [Required]
         public int AncillaryAreaKvm { get; set; } // garage, balkong, källare etc
+        [Required]
         public int LandAreaKvm { get; set; }
+        [Required]
         public string? Description { get; set; }
+        [Required]
         public int NumberOfRooms { get; set; }
+        [Required]
         public int BuildYear { get; set; }
+        [Required]
         public List<string> ImageUrls { get; set; } = null!;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public virtual Realtor? Realtor { get; set; }
--- BropertyBrosApi2.0/Models/Realtor.cs
+++ BropertyBrosApi2.0/Models/Realtor.cs
@@ -1,17 +1,25 @@
-﻿namespace BropertyBrosApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BropertyBrosApi.Models
 {
     //Author: Calvin, Daniel, Emil
     public class Realtor
     {
         public int Id { get; set; }
+        [Required]
         public string? FirstName { get; set; }
+        [Required]
         public string? LastName { get; set; }
+        [Required]
         public string? PhoneNumber { get; set; }
+        [Required]
+        [EmailAddress]
         public string? Email { get; set; }
+        [Required]
         public string? ProfileUrl { get; set; }
         public virtual List<Property> Properties { get; set; } = new List<Property>();
 
         public virtual RealtorFirm? RealtorFirm { get; set; }
-        public int RealtorFirmId { get; set; } 
+        public int RealtorFirmId { get; set; }
     }
 }
